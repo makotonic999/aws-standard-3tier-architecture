787a6c7aea6329a4a9681c18765420ab38307dde
岡田誠 / OKADA，MAKOTO <makoto.okada.ak@ps.hitachi-solutions.com>
2026-05-12 16:44:10 +0900

    feat: VPCとSubnetを定義 #1

---
+++ main.tf
@@ -0,0 +1,18 @@
+# VPCを作る
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "standard-3tier-vpc"
+    }
+}
+
+resource "aws_subnet" "public_1a" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "ap-northeast-1a"
+
+    tags = {
+        Name = "standard-public_1a"
+    }
+}
