771fa51fe7574ef48fea8f96c8aeaab7f1b2d655
岡田誠 / OKADA，MAKOTO <makoto.okada.ak@ps.hitachi-solutions.com>
2026-05-19 11:18:17 +0900

    feat: インターネット接続環境の設定 #2

--- main.tf
+++ main.tf
@@ -25,3 +25,24 @@ resource "aws_internet_gateway" "main" {
         Name = "standard-igw"
     }
 }
+
+# Route Table
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    # デフォルトルート
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {
+        Name = "standard-public-rt"
+    }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "public_1a" {
+    subnet_id = aws_subnet.public_1a.id
+    route_table_id = aws_route_table.public.id
+}
