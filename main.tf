432768108886b6bb4776f96029833d0f41a649ea
岡田誠 / OKADA，MAKOTO <makoto.okada.ak@ps.hitachi-solutions.com>
2026-05-20 15:35:59 +0900

    feat: アプリ・DB用のプライベートサブネットを構築 #3

--- main.tf
+++ main.tf
@@ -1,48 +1,93 @@
 # VPCを作る
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-    tags = {
-        Name = "standard-3tier-vpc"
-    }
+  tags = {
+    Name = "standard-3tier-vpc"
+  }
 }
 
 resource "aws_subnet" "public_1a" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "ap-northeast-1a"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
 
-    tags = {
-        Name = "standard-public_1a"
-    }
+  tags = {
+    Name = "standard-public_1a"
+  }
 }
 
 # Internet Gateway
 resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    tags = {
-        Name = "standard-igw"
-    }
+  tags = {
+    Name = "standard-igw"
+  }
 }
 
 # Route Table
 resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    # デフォルトルート
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main.id
-    }
+  # デフォルトルート
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
 
-    tags = {
-        Name = "standard-public-rt"
-    }
+  tags = {
+    Name = "standard-public-rt"
+  }
 }
 
 # Route Table Association
 resource "aws_route_table_association" "public_1a" {
-    subnet_id = aws_subnet.public_1a.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Issue #3: プライベートサブネット (App / DB)
+
+# 1. アプリ (AP) 用サブネット
+resource "aws_subnet" "private_app_1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.20.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "standard-private-app-1a"
+  }
+}
+
+# 2. データベース (DB) 用サブネット
+resource "aws_subnet" "private_db_1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.20.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "standard-private-db-1a"
+  }
+}
+
+# 3. プライベート用ルートテーブル
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "standard-private-rt"
+  }
+}
+
+# 4. ルートテーブルの紐づけ (App)
+resource "aws_route_table_association" "private_app_1a" {
+  subnet_id      = aws_subnet.private_app_1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+# 5. ルートテーブルの紐づけ (DB)
+resource "aws_route_table_association" "private_db_1a" {
+  subnet_id      = aws_subnet.private_db_1a.id
+  route_table_id = aws_route_table.private.id
 }
