8194e195688eb6edfa5443b5e5300a77cc32caa3
岡田誠 / OKADA，MAKOTO <makoto.okada.ak@ps.hitachi-solutions.com>
2026-06-26 14:03:32 +0900

    fix: modules/vpc/main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -163,5 +163,5 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.ap-northeast-1.s3"
   vpc_endpoint_type = "Gateway" 
-  route_table_ids   = [aws_vpc.main.id] 
+  route_table_ids   = [aws_route_table.private.id] 
 }
